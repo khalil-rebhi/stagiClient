5ceccc5c1b938dec6428e630c06ab6b92b6a7df1
Skander <msalmi.skander@esprit.tn>
2022-12-20 22:42:35 +0100

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,15 @@ import { AuthGuard } from './shared/guards/auth.guard';
 import { DataUserGuard } from './shared/guards/data-user.guard';
 
 const routes: Routes = [{ path: 'stages',canActivate:[DataUserGuard,AuthGuard], loadChildren: () => import('./stages/stages.module').then(m => m.StagesModule) },
- { path: 'users',canActivate:[DataUserGuard], loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'formations',canActivate:[DataUserGuard], loadChildren: () => import('./formations/formations.module').then(m => m.FormationsModule) },
-   { path: 'bibliotheques',canActivate:[DataUserGuard], loadChildren: () => import('./bibliothequess/bibliothequess.module').then(m => m.BibliothequessModule) },
+ { path: 'users',canActivate:[DataUserGuard,AuthGuard], loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  { path: 'formations',canActivate:[DataUserGuard,AuthGuard], loadChildren: () => import('./formations/formations.module').then(m => m.FormationsModule) },
+   { path: 'bibliotheques',canActivate:[DataUserGuard,AuthGuard], loadChildren: () => import('./bibliothequess/bibliothequess.module').then(m => m.BibliothequessModule) },
     { path: 'quiz',canActivate:[DataUserGuard], loadChildren: () => import('./quizs/quizs.module').then(m => m.QuizsModule) },
     {path:'',canActivate:[DataUserGuard],component:HomeComponent},
   {path:'connexion',canActivate:[DataUserGuard],component:ConnexionComponent},
   {path:'inscription',canActivate:[DataUserGuard],component:InscriptionComponent},
-  {path:'profile',canActivate:[DataUserGuard,AuthGuard],component:ProfileComponent}
+  {path:'profile',canActivate:[DataUserGuard,AuthGuard],component:ProfileComponent},
+  {path:'**',canActivate:[DataUserGuard],component:HomeComponent},
   ];
 
 @NgModule({
